8dd7c6976d59b441ce43f39109b80e2864f26a1d
hietpasd <hietpasd@uwosh.edu>
2016-04-05 08:08:25 -0500

    fix

--- polklibrary.content.transfer/src/polklibrary/content/transfer/browser/export.py
+++ polklibrary.content.transfer/src/polklibrary/content/transfer/browser/export.py
@@ -4,9 +4,52 @@ import json
 
 class ExportView(BrowserView):
 
+    data = {}
+
+
     def __call__(self):
+        self.data = {}
     
+        self._general()
+        self._folder()
+        self._page()
+        self._image()
         
+        
+        return json.dumps(self.data)
+
+        
+    def _general(self):
+        data['Title'] = self.context.Title
+        data['Description'] = self.context.Description
+        data['portal_type'] = self.context.portal_type
+        data['Creator'] = self.context.Creator
+        data['location'] = self.context.location
+        data['getRemoteUrl'] = self.context.getRemoteUrl
+        
+        
+    def _folder(self):
+        if context.portal_type == 'Folder':
+            return
     
     
-        return json.dumps({'test':'status'})
+    def _page(self):
+        if context.portal_type == 'Document':
+            return
+    
+        data['body_raw'] = self.context.getRawText()
+        data['body'] = self.context.getText()
+        
+        
+    def _image(self):
+        if context.portal_type == 'Image':
+            return
+        
+        
+        
+        
+        
+        
+        
+        
+        
